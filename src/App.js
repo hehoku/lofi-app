a3bd2d19340b42f4a96436188aee876fe8e9d203
Hehoku <hehoku@outlook.com>
2022-01-19 08:50:29 +0800

    support upvote then update upvoteCount

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import { ethers } from 'ethers'
 import abi from './utils/Lofi.json'
 import Spotify from 'react-spotify-embed'
 
+const contractAddress = '0xfaeef3203051a918d9d087ff13c1df792a238cc7'
+const contractABI = abi.abi
+
 function App () {
   const [currentAccount, setCurrentAccount] = useState('')
   const [lofiInputValue, setLofiInputValue] = useState('')
   const [lofis, setLofis] = useState([])
-  const contractAddress = '0xfaeef3203051a918d9d087ff13c1df792a238cc7'
-  const contractABI = abi.abi
 
   const getLofis = useCallback(async () => {
     const { ethereum } = window
@@ -29,7 +30,7 @@ function App () {
     } catch (error) {
       console.log(error)
     }
-  }, [contractABI, contractAddress])
+  }, [])
 
   // check if wallet is connected
   // useCallback is used to prevent re-rendering
@@ -148,21 +149,31 @@ function App () {
         </button>
       )}
       {/* show lofis */}
-      <div className='flex flex-row content-center mt-10'>
-        <Lofis lofis={lofis} />
+      <div className='flex flex-row content-center my-10'>
+        <Lofis lofis={lofis} handleGetLofisFromApp={getLofis} />
       </div>
     </div>
   )
 }
 
-const Lofis = ({ lofis }) => {
+const Lofis = ({ lofis, handleGetLofisFromApp }) => {
+  const handleGetLofis = () => {
+    handleGetLofisFromApp()
+  }
   return (
     lofis.length > 0 &&
-    lofis.map(lofi => <LofiItem key={lofi.timestamp.toString()} lofi={lofi} />)
+    lofis.map((lofi, index) => (
+      <LofiItem
+        key={lofi.timestamp.toString()}
+        index={index}
+        lofi={lofi}
+        handleGetLofisFromLofis={handleGetLofis}
+      />
+    ))
   )
 }
 
-const LofiItem = ({ lofi }) => {
+const LofiItem = ({ lofi, index, handleGetLofisFromLofis }) => {
   function isValidUrl (string) {
     let url
 
@@ -174,12 +185,50 @@ const LofiItem = ({ lofi }) => {
 
     return url.protocol === 'http:' || url.protocol === 'https:'
   }
+  // handle upvote then get all lofis
+  const handleUpvote = async index => {
+    try {
+      const { ethereum } = window
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum)
+        const signer = provider.getSigner()
+        const lofiContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        )
+
+        const upvoteTxn = await lofiContract.upvote(index)
+        console.log('upvoting...', upvoteTxn.hash)
+        await upvoteTxn.wait()
+        console.log('upvoted', upvoteTxn.hash)
+
+        // inform user that upvote was successful
+        handleGetLofisFromLofis()
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <div key={lofi.timestamp.toString()}>
       {isValidUrl(lofi.lofiUrl.trim()) ? (
-        <div className='mx-10'>
+        <div className='mx-10 flex flex-col items-center'>
           <Spotify link={lofi.lofiUrl.trim()} />
+          <p>{lofi.submitter}</p>
+          <p>upvote count {lofi.upvoteCount.toNumber()}</p>
+          <p>{index}</p>
+          <button
+            className='btn bg-blue-600 w-40 h-12 text-white rounded-lg my-4'
+            onClick={handleUpvote.bind(this, index)}
+          >
+            Upvote
+          </button>
+          <button className='btn bg-blue-600 w-40 h-12 text-white rounded-lg'>
+            Send tip
+          </button>
         </div>
       ) : null}
     </div>
